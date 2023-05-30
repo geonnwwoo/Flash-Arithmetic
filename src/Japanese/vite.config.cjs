100b2de96b251985006b5317883478b6a6117d31
Geonwoo Kim <gwkim973@gmail.com>
2023-05-29 17:19:41 -0700

    English -> Game  and removed Japanese folder

--- src/Japanese/vite.config.cjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-const path=require('path');
-
-export default defineConfig({
-  base: path.resolve(__dirname, './dist'),
-  plugins: [vue()]
-});
--- vuecompile.sh
+++ vuecompile.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cd src/English || exit 1
 npm run build || exit 1
-cd ../Japanese || exit 1
-npm run build || exit 1
 cd ../.. || exit 1
 
 npm start || exit 1
